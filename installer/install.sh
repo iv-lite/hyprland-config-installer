8b8d54e441b2de191b992bcd08c309b46a00f01a
iv-lite <vibekwe@proton.me>
2024-09-06 15:27:58 +0200

    Change -  greetd is now enabled by install script

--- installer/install.sh
+++ installer/install.sh
@@ -1,5 +1,3 @@
 hyprpm add https://github.com/Duckonaut/split-monitor-workspaces --force
 hyprpm enable split-monitor-workspaces --force
 hyprpm reload --force
-
-sudo systemctl enable --now greetd.service --force
