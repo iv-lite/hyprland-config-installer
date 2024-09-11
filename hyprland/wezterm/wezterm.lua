186475d92eb289aefe4d2b86f01838b7425a5f79
iv-lite <ibekwe.victor@gmail.com>
2024-09-11 06:32:33 +0200

    Change - remove wezterm font definition

--- hyprland/wezterm/wezterm.lua
+++ hyprland/wezterm/wezterm.lua
@@ -5,7 +5,6 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.font = wezterm.font 'JetBrains Mono'
 config.colors = {
     tab_bar = {
         active_tab = {
