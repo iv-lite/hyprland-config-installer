1ad74e01e3d9f5ac40ad300f00221af148d9783d
iv-lite <ibekwe.victor@gmail.com>
2025-09-07 14:09:32 +0200

    Change - improved keyring-reset script

--- tools/keyring-reset.sh
+++ tools/keyring-reset.sh
@@ -1,10 +1,16 @@
+#!/usr/bin/env bash
 
+cd "$(dirname $0)"
+
+sudo cp ../etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist
+
+killall gpg-agent
 sudo rm -rf /etc/pacman.d/gnupg
 sudo rm -rf /root/.gnupg
 sudo rm -rf $HOME/.gnupg
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
-sudo pacman -Sy gnupg archlinux-keyring
-sudo pacman-key --refresh-keys
+sudo pacman -Syy gnupg archlinux-keyring
+# sudo pacman-key --refresh-keys
 sudo pacman -Sc
 sudo pacman -Syyu
