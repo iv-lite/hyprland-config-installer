adc270cd6cb52b53703b1d8a0d7a1dfdbc1fdbb1
Victor Ibekwe <ibekwe.victor@gmail.com>
2025-09-11 18:40:28 +0200

    Fix - multiple refinements

--- installer/install.sh
+++ installer/install.sh
@@ -3,5 +3,5 @@ hyprpm update
 hyprpm add https://github.com/shezdy/hyprsplit
 hyprpm enable hyprsplit
 
-hyprpm add https://github.com/KZDKM/Hyprspace
-hyprpm enable Hyprspace
+# hyprpm add https://github.com/KZDKM/Hyprspace
+# hyprpm enable Hyprspace
